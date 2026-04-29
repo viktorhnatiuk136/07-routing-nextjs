8f8d50e43ca3b53f4573a651f61175b2b733dc9b
viktor hnatiuk <viktorgnatiuk136@gmail.com>
2026-04-29 15:27:34 +0300

    hw 7 add client file

--- app/@modal/(.)notes/[id]/page.tsx
+++ app/@modal/(.)notes/[id]/page.tsx
@@ -1,15 +1,5 @@
-"use client";
+import NotePreview from "./NotePreview.client";
 
-import { useRouter } from "next/navigation";
-
-import Modal from "@/components/Modal/Modal";
-import NoteDetailsClient from "@/app/notes/[id]/NoteDetails.client";
-
-export default function NotePreview() {
-  const router = useRouter();
-  return (
-    <Modal onClose={() => router.back()}>
-      <NoteDetailsClient />
-    </Modal>
-  );
+export default function Page() {
+  return <NotePreview />;
 }
